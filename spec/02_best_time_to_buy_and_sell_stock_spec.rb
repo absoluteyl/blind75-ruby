1e29d244699ade1da1624b1081165b7fea0b10e0
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-15 11:53:13 +0800

    feat: prepare 02 problem

---
+++ spec/02_best_time_to_buy_and_sell_stock_spec.rb
@@ -0,0 +1,31 @@
+class BestTimeBuyAndSell
+
+  attr_accessor :prices
+
+  def max_profix
+  end
+end
+
+RSpec.describe BestTimeBuyAndSell, "#max_profix" do
+  before(:each) do
+    @best_time_buy_and_sell = BestTimeBuyAndSell.new
+  end
+
+  def given(prices)
+    @best_time_buy_and_sell.prices = prices
+  end
+
+  def should_return(expected)
+    expect(@best_time_buy_and_sell.max_profix).to eq(expected)
+  end
+
+  it do
+    given([7,1,5,3,6,4])
+    should_return(5)
+  end
+
+  it do
+    given([7,6,4,3,1])
+    should_return(0)
+  end
+end
