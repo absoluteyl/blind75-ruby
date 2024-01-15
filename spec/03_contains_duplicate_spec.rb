2e4c761014b06c8dbfe560de3df0d075c69f0c89
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-15 14:27:35 +0800

    feat: prepare 03 contains duplicate

---
+++ spec/03_contains_duplicate_spec.rb
@@ -0,0 +1,37 @@
+class ContainsDuplicate
+
+  attr_accessor :nums
+
+  def contains_duplicate
+
+  end
+end
+
+RSpec.describe ContainsDuplicate, "#contains_duplicate" do
+  before(:each) do
+    @contains_duplicate = ContainsDuplicate.new
+  end
+
+  def given(nums)
+    @contains_duplicate.nums = nums
+  end
+
+  def should_return(expected)
+    expect(@contains_duplicate.contains_duplicate).to eq(expected)
+  end
+
+  it do
+    given([1,2,3,1])
+    should_return(true)
+  end
+
+  it do
+    given([1,2,3,4])
+    should_return(false)
+  end
+
+  it do
+    given([1,1,1,3,3,4,3,2,4,2])
+    should_return(true)
+  end
+end
